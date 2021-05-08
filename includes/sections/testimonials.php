81d96f5e950538d2a7a0fb0f826cb182c1d0d8d5
KAROPASHIKYAN <notfound625@gmail.com>
2021-05-09 02:10:13 +0300

    html to wp

--- includes/sections/testimonials.php
+++ includes/sections/testimonials.php
@@ -1,62 +1,22 @@
 <div id="testimonials" class="testimonials-section">
     <div class="container">
-        <h2 class="section-title text-center">Many Happy Customers</h2>
-        <div class="item center-block">
+        <h2 class="section-title text-center"><? the_sub_field('title');?></h2>
+        <?php $comments = get_sub_field('comments'); ?>
+        <?php foreach ($comments as $key => $comment): ?>
+        <div class="item <?= $key == 0 || $key % 2 == 0 ? '': 'item-reversed';?> center-block">
             <div class="profile-holder">
-                <img class="profile-image" src="<?= get_stylesheet_directory_uri()?>/assets/images/profile-1.png" alt="profile">
+                <img class="profile-image" src="<?= $comment['ava']['url']; ?>" alt="profile">
             </div>
             <div class="quote-holder">
                 <blockquote class="quote">
-                    <p>Testimonial goes here Donec felis odio, sagittis eu cursus ac, porttitor eu purus. In a bibendum dui. Nullam id est sed felis rutrum tincidunt eu nec nisi morbi euismod semper neque sed lobortis.</p>
+                    <p><?= $comment['quote']; ?></p>
                     <div class="quote-source">
-                        <span class="name">@JohnK,</span>
-                        <span class="meta">San Francisco</span>
+                        <span class="name">@<?= $comment['name'];?>,</span>
+                        <span class="meta"><?= $comment['location'];?></span>
                     </div><!--//quote-source-->
                 </blockquote>
             </div><!--//quote-holder-->
         </div><!--//item-->
-        <div class="item item-reversed center-block">
-            <div class="profile-holder">
-                <img class="profile-image" src="<?= get_stylesheet_directory_uri()?>/assets/images/profile-2.png" alt="profile">
-            </div>
-            <div class="quote-holder">
-                <blockquote class="quote">
-                    <p>Testimonial goes here fermentum sed pharetra in, aliquet sodales quam. Ut sed turpis quis orci viverra aliquet interdum ut ipsum. </p>
-                    <div class="quote-source">
-                        <span class="name">@LisaWhite,</span>
-                        <span class="meta">London</span>
-                    </div><!--//quote-source-->
-                </blockquote>
-            </div><!--//quote-holder-->
-        </div><!--//item-->
-        <div class="item center-block">
-            <div class="profile-holder">
-                <img class="profile-image" src="<?= get_stylesheet_directory_uri()?>/assets/images/profile-3.png" alt="profile">
-            </div>
-            <div class="quote-holder">
-                <blockquote class="quote">
-                    <p>Testimonial goes here vestibulum non hendrerit lorem, luctus tincidunt erat. Sed pharetra aliquam posuere. Pellentesque sollicitudin.</p>
-                    <div class="quote-source">
-                        <span class="name">@MattH,</span>
-                        <span class="meta">Berlin</span>
-                    </div><!--//quote-source-->
-                </blockquote>
-            </div><!--//quote-holder-->
-        </div><!--//item-->
-        <div class="item item-reversed center-block">
-            <div class="profile-holder">
-                <img class="profile-image" src="<?= get_stylesheet_directory_uri()?>/assets/images/profile-4.png" alt="profile">
-            </div>
-            <div class="quote-holder">
-                <blockquote class="quote">
-                    <p>Testimonial goes here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis</p>
-                    <div class="quote-source">
-                        <span class="name">@RyanW,</span>
-                        <span class="meta">Paris</span>
-                    </div><!--//quote-source-->
-                </blockquote>
-
-            </div><!--//quote-holder-->
-        </div><!--//item-->
+        <?php endforeach; ?>
     </div><!--//container-->
 </div><!--//testimonials-->
