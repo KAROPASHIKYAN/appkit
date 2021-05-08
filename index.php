cd21f7afe417519802a34c807b3c77b12db5c775
KAROPASHIKYAN <notfound625@gmail.com>
2021-05-09 00:46:50 +0300

    code review

--- index.php
+++ index.php
@@ -1,47 +1,26 @@
 <?php get_header();
 
+if (have_rows('sections')):
 
-$countMenuItem = (int) 0;
+    while (have_rows('sections')): the_row();
 
-if ( have_rows('sections') ): $countMenuItem ++;
-
-while ( have_rows( 'sections') ): the_row();
-
-    if (get_row_layout() == 'hero') {
-        get_template_part ('includes/sections/hero');
+        if (get_row_layout() == 'hero') {
+            sections('hero');
+        } elseif (get_row_layout() == 'about') {
+            sections('about');
+        } elseif (get_row_layout() == 'contact') {
+            sections('contact');
+        } elseif (get_row_layout() == 'features') {
+            sections('features');
+        } elseif (get_row_layout() == 'pricing') {
+            sections('pricing');
+        } elseif (get_row_layout() == 'team') {
+            sections('team');
+        } elseif (get_row_layout() == 'testimonials') {
+            sections('testimonials');
         }
-    elseif (get_row_layout() == 'about') {
-        get_template_part ('includes/sections/about');
-        }
-    elseif (get_row_layout() == 'contract') {
-        get_template_part ('includes/sections/contract');
-        }
-    elseif (get_row_layout() == 'features') {
-        get_template_part ('includes/sections/features');
-        }
-    elseif (get_row_layout() == 'pricing') {
-        get_template_part ('includes/sections/pricing');
-        }
-    elseif (get_row_layout() == 'team') {
-        get_template_part('includes/sections/team');
-        }
-    elseif (get_row_layout() == 'testimonials') {
-        get_template_part('includes/sections/testimonials');
-    }
 
     endwhile;
-    endif;
+endif;
 
-$sections = get_field ('sections');
-b($sections);
-sections('hero');
-
-
-sections('about');
-sections('testimonials');
-sections('features');
-sections('team');
-sections('pricing');
-sections('contact');
-
-get_footer(); ?>
+get_footer();
