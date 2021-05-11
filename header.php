6febeba16372d34b2b71d0962a955b5c97a77f0e
KAROPASHIKYAN <notfound625@gmail.com>
2021-05-11 22:36:14 +0300

    html to wp

--- header.php
+++ header.php
@@ -1,7 +1,13 @@
+<?php
+$sections = get_field('sections');
+$str = get_bloginfo();
+$st = logo($str);
+?>
 <!DOCTYPE html>
 <!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
 <!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
 <!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
+
 <head>
     <!-- Meta -->
     <meta charset="utf-8">
@@ -11,7 +17,6 @@
     <meta name="author" content="Xiaoying Riley at 3rd Wave Media">
     <link rel="shortcut icon" href="<?= get_stylesheet_directory_uri()?>/favicon.ico">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
     <?php wp_head(); ?>
 </head>
 
@@ -20,9 +25,13 @@
 <header id="header" class="header">
     <div class="container">
         <h1 class="logo">
-            <a class="scrollto" href="#hero">
+            <?php if (!empty($sections)): ?>
+                <?php foreach ($sections as $key => $section): ?>
+                 <a class="scrollto" href="#<?= $section['acf_fc_layout']; ?>">
+                <?php endforeach; ?>
+            <?php endif; ?>
                 <span class="logo-icon-wrapper"><img class="logo-icon" src="<?= get_stylesheet_directory_uri()?>/assets/images/logo-icon.svg" alt="icon"></span>
-                <span class="text"><span class="highlight">APP</span>KIT</span></a>
+                <span class="text"><span class="highlight"><?= $st[0];?></span><?= $st[1];?></span></a>
         </h1><!--//logo-->
         <nav class="main-nav navbar-right" role="navigation">
             <div class="navbar-header">
@@ -35,12 +44,9 @@
             </div><!--//navbar-header-->
             <div id="navbar-collapse" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
-                    <li class="active nav-item"><a class="scrollto" href="#about">About</a></li>
-                    <li class="nav-item"><a class="scrollto" href="#testimonials">Testimonials</a></li>
-                    <li class="nav-item"><a class="scrollto" href="#features">Features</a></li>
-                    <li class="nav-item"><a class="scrollto" href="#team">Team</a></li>
-                    <li class="nav-item"><a class="scrollto" href="#pricing">Pricing</a></li>
-                    <li class="nav-item"><a class="scrollto" href="#contact">Contact</a></li>
+                    <?php foreach ($sections as $key => $section): ?>
+                        <li class="<?= $key == 0? 'active':'';?> nav-item"><a class="scrollto" href="#<?= $section['acf_fc_layout']; ?>"><?= $section['section_name']; ?></a></li>
+                    <?php endforeach; ?>
                 </ul><!--//nav-->
             </div><!--//navabr-collapse-->
         </nav><!--//main-nav-->
