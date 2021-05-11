c7014bd1a236c1d777c3b940dd09b19de098e668
KAROPASHIKYAN <notfound625@gmail.com>
2021-05-11 22:41:33 +0300

    code refactoring

--- includes/sections/contact.php
+++ includes/sections/contact.php
@@ -2,9 +2,11 @@
     <div class="container text-center">
         <h2 class="section-title"><?the_sub_field ('title');?></h2>
         <div class="contact-content">
-            <p><?the_sub_field ('content');?></p>
+            <p><?php the_sub_field ('content');?></p>
         </div>
         <?php $button = get_sub_field ('button');?>
-        <a class="btn btn-cta btn-primary" href="<?= $button['url'] ;?>"><?= $button['title'] ;?></a>
+        <?php if (!empty($button)): ?>
+            <a class="btn btn-cta btn-primary" href="<?= $button['url'] ;?>"><?= $button['title'] ;?></a>
+        <?php endif; ?>
     </div><!--//container-->
 </div><!--//contact-section-->
