da86f7515be6c6fa420fa62e0af62de352671652
KAROPASHIKYAN <notfound625@gmail.com>
2021-05-11 23:17:53 +0300

    code refactoring

--- includes/sections/about.php
+++ includes/sections/about.php
@@ -1,18 +1,24 @@
-<?php $about = get_sub_field ('about'); ?>
-<?php $galery = get_sub_field ('gallery');?>
+<?php
+$about = get_sub_field ('about');
+$galery = get_sub_field ('gallery');
+$items = get_sub_field('items');
+?>
 <div id="about" class="about-section">
     <div class="container text-center">
         <h2 class="section-title"><?php the_sub_field('title') ;?></h2>
         <p class="intro"><?php the_sub_field('intro') ;?></p>
         <ul class="technologies list-inline">
-            <?php foreach ($galery as $logo): ?>
-            <li><img src="<?= $logo['url']; ?>" alt="<?=$logo['alt']; ?>"></li>
-            <?php endforeach; ?>
+            <?php if (!empty($galery)): ?>
+                <?php foreach ($galery as $logo): ?>
+                    <li><img src="<?= $logo['url']; ?>" alt="<?=$logo['alt']; ?>"></li>
+                 <?php endforeach; ?>
+            <?php endif; ?>
         </ul>
-        <?php $items = get_sub_field('items'); ?>
+
         <div class="items-wrapper row">
-            <?php foreach ($items as $item): ?>
-                <div class="item col-sm-4 col-xs-12">
+            <?php if (!empty($items)): ?>
+                <?php foreach ($items as $item): ?>
+                 <div class="item col-sm-4 col-xs-12">
                     <div class="item-inner">
                         <div class="figure-holder">
                             <img class="figure-image" src="<?= $item['image']['url']; ?>" alt="image">
@@ -23,7 +29,8 @@
                         </div><!--//item-desc-->
                     </div><!--//item-inner-->
                 </div><!--//item-->
-            <?php endforeach; ?>
+                <?php endforeach; ?>
+            <?php endif;?>
         </div><!--//items-wrapper-->
     </div><!--//container-->
 </div><!--//about-section-->
